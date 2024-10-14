06a71c012a01918bfc1f63288c08f3e375b3a5b3
Sai Naveen Pulivarthi <sainaveen1209@gmail.com>
2024-10-14 16:43:27 +0530

    Add files via upload

--- streamlit_app/Home.py
+++ streamlit_app/Home.py
@@ -4,11 +4,11 @@ import joblib
 import streamlit as st
 
 # Load the saved model and encoded columns
-model = joblib.load(r'C:\Users\saina\OneDrive\Desktop\PROJECT 3\xgboost_ml_model.pkl')
-encoded_columns = joblib.load(r'C:\Users\saina\OneDrive\Desktop\PROJECT 3\encoded_columns.pkl')
+model = joblib.load("../xgboost_ml_model.pkl")
+encoded_columns = joblib.load("../encoded_columns.pkl")
 
 # Load dataset from Excel to extract unique values for dropdown options
-df_cars = pd.read_excel("C:\\Users\\saina\\OneDrive\\Desktop\\PROJECT 3\\ML_model_car_data.xlsx")
+df_cars = pd.read_excel("../Preprocessed_data.xlsx")
 
 # Define categorical columns and extract unique values
 categorical_columns = ['ft', 'bt', 'transmission', 'company', 'model', 
